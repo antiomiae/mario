00d1bfc312cc443c8f6d90e753d760465beb8b24
Kevin Ward <antiomiae@gmail.com>
2019-03-20 23:04:39 -0700

    Rework velocity to be in px/frame

--- player.gd
+++ player.gd
@@ -2,15 +2,23 @@ extends KinematicBody2D
 	
 enum MotionState {WALKING, JUMPING}
 
-const WALK_FRICTION = 15 * 60
-const WALK_SPEED = 3.5 * 60
-const WALK_ACCELERATION = 6 * 60
-const JUMP_SPEED = 6 * 60
-const AIR_WALK_SPEED = 4 * 60
-const AIR_WALK_ACCELERATION = 3 * 60
-const AIR_WALK_FRICTION = 4 * 60
+const SLOW_WALK_SPEED = 1.5625
+const SLOW_WALK_ACCELERATION = 0.037109375
 
-const GRAVITY = 15 * 60
+#const WALK_FRICTION = 0.05078125
+const WALK_FRICTION = 0.1015625
+const WALK_SPEED = 3.5
+const WALK_ACCELERATION = 0.0556640625
+#const WALK_ACCELERATION = 0.89
+const JUMP_SPEED = 5
+const MAX_FALL_SPEED = 4.5
+const CLAMP_FALL_SPEED = 4.0
+const AIR_WALK_SPEED = 3.5
+const AIR_WALK_ACCELERATION = 0.0556640625
+const AIR_WALK_FRICTION = 0
+
+const GRAVITY = 0.15625 # 0.1
+const HIGH_GRAVITY = 0.5625
 const UP = Vector2(0, -1)
 
 var motion_state = MotionState.WALKING
@@ -18,20 +26,12 @@ var velocity : Vector2 = Vector2(0, 0)
 var has_jumped : bool = false
 
 onready var camera = get_parent().get_node("camera")
-onready var tilemap = get_parent().get_node("TileMap")
 
 func _ready():
 	pass
 	
 
 func _physics_process(delta):
-	
-	var state = {
-		"ceiling": is_on_ceiling(),
-		"floor": is_on_floor(),
-		"wall": is_on_wall()	
-	}
-	
 	match motion_state:
 		MotionState.WALKING:
 			movement_walking(delta)
@@ -39,14 +39,13 @@ func _physics_process(delta):
 			movement_jumping(delta)
 
 	camera.offset = position - Vector2(320,240)/2
-	#camera.align()
 
 func movement_walking(delta):	
 	horizontal_input(WALK_SPEED, WALK_ACCELERATION, WALK_FRICTION, delta)
 	velocity.x = clamp(velocity.x, -WALK_SPEED, WALK_SPEED)
-	velocity.y = GRAVITY * delta
+	velocity.y = GRAVITY
 	
-	velocity = move_and_slide(velocity, UP)
+	velocity = move_and_slide(velocity/delta, UP)*delta
 
 	var state = {
 		"ceiling": is_on_ceiling(),
@@ -67,13 +66,16 @@ func movement_jumping(delta):
 	if !has_jumped:
 		velocity.y = -JUMP_SPEED
 		has_jumped = true
-	elif Input.is_action_just_released("jump"):
-		velocity.y = max(-JUMP_SPEED/3.0, velocity.y)
+	elif !Input.is_action_pressed("jump"):
+		velocity.y += HIGH_GRAVITY		
 	else:
-		velocity.y += GRAVITY * delta
+		velocity.y += GRAVITY
+		
+	if velocity.y > MAX_FALL_SPEED:
+		velocity.y = CLAMP_FALL_SPEED
 	
-	horizontal_input(AIR_WALK_SPEED, AIR_WALK_ACCELERATION, AIR_WALK_FRICTION, delta)
-	velocity = move_and_slide(velocity, UP)
+	horizontal_input(AIR_WALK_SPEED, AIR_WALK_ACCELERATION, 0, delta)
+	velocity = move_and_slide(velocity/delta, UP)*delta
 	
 	if is_on_floor():
 		has_jumped = false
@@ -91,14 +93,13 @@ func horizontal_input(speed, acceleration, friction, delta):
 	
 	if input_dir != 0:
 		if abs(velocity.x) < speed:
-			velocity.x += sign(input_dir) * acceleration * delta
-	else:
+			velocity.x += sign(input_dir) * acceleration
+	elif abs(velocity.x) > 0 and friction != 0:
 		# apply drag to slowdown character to a stop
-		if abs(velocity.x) > 0:
-			if abs(velocity.x) * delta < 1:
-				velocity.x = 0
-			else:
-				velocity.x -= sign(velocity.x) * friction * delta
+		if abs(velocity.x) <= friction:
+			velocity.x = 0
+		else:
+			velocity.x -= sign(velocity.x) * friction
 
 func is_walk_pressed() -> bool:
 	return Input.is_action_pressed("walk_left") || Input.is_action_pressed("walk_right")
