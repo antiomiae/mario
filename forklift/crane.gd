ece6ee73dd00804350152f25aec577739709445e
Kevin Ward <antiomiae@gmail.com>
2019-05-27 22:19:09 -0700

    .

--- forklift/crane.gd
+++ forklift/crane.gd
@@ -10,7 +10,7 @@ func adjust_pincer(dir = 1, amount = 0.1):
     for spring in springs:
         spring.rest_length = spring.rest_length + dir*amount
         spring.rest_length = clamp(spring.rest_length, spring_rest_length, spring_rest_length + spring_displacement)
-        spring.length = spring.rest_length
+        #spring.length = spring.rest_length
 
 func _physics_process(delta: float) -> void:
     if Input.is_key_pressed(KEY_SHIFT):
@@ -20,6 +20,12 @@ func _physics_process(delta: float) -> void:
             adjust_pincer(-1)
     else:
         if Input.is_action_pressed('up'):
-            self.position.y -= 0.5
+            $"../anchor/DampedSpringJoint2D".rest_length -= 0.5
+            #self.position.y -= 0.5
         elif Input.is_action_pressed('down'):
-            self.position.y += 0.5
+            $"../anchor/DampedSpringJoint2D".rest_length += 0.5
+            #self.position.y += 0.5
+        $"../anchor/DampedSpringJoint2D".rest_length = max(1, $"../anchor/DampedSpringJoint2D".rest_length)
+
+        var x_input = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+        $"../anchor".position.x += 0.5 * x_input
