e746478143cee924fc9b04f50f316c5acf29f8bd
Kevin Ward <antiomiae@gmail.com>
2019-05-30 16:24:27 -0700

    .

--- forklift/claw.gd
+++ forklift/claw.gd
@@ -8,7 +8,6 @@ onready var pincer_spring = $left_pincer/pincer_spring
 func close_claw(dir = 1, amount = 1):
     pincer_spring.rest_length = clamp(pincer_spring.rest_length + dir*amount, min_spring_length, max_spring_length)
 
-
 func _physics_process(delta: float) -> void:
     var x_input = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
     close_claw(sign(x_input), abs(x_input))
