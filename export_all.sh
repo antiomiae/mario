ed0c7eea99f6f608383f3a0b77cf39af5fd3cc2d
Kevin Ward <antiomiae@gmail.com>
2019-06-16 16:42:44 -0700

    .

--- export_all.sh
+++ export_all.sh
@@ -6,9 +6,9 @@ mkdir -p $OUTDIR/macos
 
 # build for mac
 godot --path /game-files/ --export 'Mac OSX' $OUTDIR/macos/game.app
-mv $OUTDIR/macos/game.app $OUTDIR/macos/game.zip
+mv $OUTDIR/macos/game.app $OUTDIR/macos/game-macos.zip
 
 godot --path /game-files/ --export 'Linux/X11' $OUTDIR/linux/game-linux
 
-godot --path /game-files/ --export 'Windows Desktop' $OUTDIR/windows/game.exe
+godot --path /game-files/ --export 'Windows Desktop' $OUTDIR/windows/game-win64.exe
 
