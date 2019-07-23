62473070819ea0986e3704463a1419465dbd9e0d
Kevin Ward <antiomiae@gmail.com>
2019-07-23 14:14:55 -0700

    Update build script

--- export_all.sh
+++ export_all.sh
@@ -8,7 +8,12 @@ mkdir -p $OUTDIR/macos
 godot --path /game-files/ --export 'Mac OSX' $OUTDIR/macos/game.app
 mv $OUTDIR/macos/game.app $OUTDIR/macos/game-macos.zip
 
-godot --path /game-files/ --export 'Linux/X11' $OUTDIR/linux/game-linux
+godot --path /game-files/ --export 'Linux/X11' $OUTDIR/linux/game-linux.zip
 
-godot --path /game-files/ --export 'Windows Desktop' $OUTDIR/windows/game-win64.exe
+godot --path /game-files/ --export 'Windows Desktop' $OUTDIR/windows/game-win64.zip
 
+cd $OUTDIR/macos && mv game-macos.zip ..
+
+cd $OUTDIR/linux && zip game-linux.zip game-linux* && mv game-linux.zip .. && rm game-linux*
+
+cd $OUTDIR/windows && zip game-win64.zip game-win64.* && mv game-win64.zip .. && rm game-win64.*
